d19d8680a54940e080ae72df472026055cfc2a97
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-13 01:42:43 +0530

    Add files via upload

---
+++ A5(6).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=2;
+    printf("enter the number :");
+    scanf("%d",&n);
+    for(i;i<=n;i=i+2)
+    {
+        printf("%d ",i);
+
+    }
+    return 0;
+}
